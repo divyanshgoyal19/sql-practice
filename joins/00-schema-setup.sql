6097650321af4f57e2e72e0ce9a5dd53ddeac00d
Divyansh Goyal <dgoyal2_be23@thapar.edu>
2026-08-13 15:24:17 +0530

    Create 00-schema-setup.sql

---
+++ joins/00-schema-setup.sql
@@ -0,0 +1,67 @@
+create database join3;
+use join3;
+
+CREATE TABLE customers (
+  customer_id INT,
+  customer_name VARCHAR(100),
+  country VARCHAR(50)
+);
+
+INSERT INTO customers VALUES (1, 'Alice', 'USA');
+INSERT INTO customers VALUES (2, 'Bob', 'India');
+INSERT INTO customers VALUES (3, 'Charlie', 'USA');
+INSERT INTO customers VALUES (4, 'Diana', 'Germany');
+INSERT INTO customers VALUES (5, 'Eve', 'France');
+INSERT INTO customers VALUES (6, 'Frank', 'Mexico');
+
+CREATE TABLE orders (
+  order_id INT,
+  customer_id INT,
+  order_date DATE,
+  amount DECIMAL(10,2)
+);
+
+INSERT INTO orders VALUES (101, 1, '2025-06-29', 250.50);
+INSERT INTO orders VALUES (102, 1, '2025-06-19', 130.00);
+INSERT INTO orders VALUES (103, 2, '2025-06-09', 99.99);
+INSERT INTO orders VALUES (104, 4, '2025-05-30', 540.00);
+INSERT INTO orders VALUES (105, 4, '2025-05-20', 180.50);
+INSERT INTO orders VALUES (106, NULL, '2025-05-10', 300.00);
+
+CREATE TABLE products (
+  product_id INT,
+  product_name VARCHAR(100),
+  category VARCHAR(50) );
+
+INSERT INTO products VALUES (201, 'Phone', 'Electronics');
+INSERT INTO products VALUES (202, 'Laptop', 'Electronics');
+INSERT INTO products VALUES (203, 'Tablet', 'Electronics');
+INSERT INTO products VALUES (204, 'Charger', 'Accessories');
+
+
+CREATE TABLE order_items (
+  order_id INT,
+  product_id INT,
+  quantity INT
+);
+
+INSERT INTO order_items VALUES (101, 201, 1);
+INSERT INTO order_items VALUES (101, 202, 2);
+INSERT INTO order_items VALUES (102, 204, 1);
+INSERT INTO order_items VALUES (104, 201, 3);
+INSERT INTO order_items VALUES (105, 203, 1);
+
+
+
+CREATE TABLE employees (
+  emp_id INT,
+  name VARCHAR(100),
+  manager_id INT,
+  department VARCHAR(50)
+);
+
+INSERT INTO employees VALUES (1, 'John', NULL, 'Sales');
+INSERT INTO employees VALUES (2, 'Jane', 1, 'Sales');
+INSERT INTO employees VALUES (3, 'Mike', 1, 'IT');
+INSERT INTO employees VALUES (4, 'Emily', 2, 'IT');
+INSERT INTO employees VALUES (5, 'Anna', NULL, 'HR');
